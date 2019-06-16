32a2b13e2b16354e8cf7cd5dca96ce4856b66ef4
Andrey Miskov <amiskov@gmail.com>
2019-06-16 08:47:37 +0300

    Finish htdd section

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    title: 'How to Code',
-    description: '',
-    base: '/mooc_htdp/',
-    themeConfig: {
-        sidebar: [
-            '/',
-            '00_introduction.md',
-            '1a_bsl/',
-            '1b_htdf/',
-            '2_htdd/'
-        ]
-    }
+  title: 'How to Code',
+  head: [
+    ['link', { rel: 'icon', href: './favicon.ico' }]
+  ],
+  description: '',
+  base: '/mooc_htdp/',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '00_introduction.md',
+      '1a_bsl/',
+      '1b_htdf/',
+      '2_htdd/'
+    ]
+  }
 }
